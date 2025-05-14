5c8d62bdb27e14e4a9b06d2266e897bb688a72a3
PhuocHanhh <161794288+PhuocHanhh@users.noreply.github.com>
2025-05-14 16:09:00 +0700

    Update ResetPasswordRequest.java

--- src/main/java/com/example/myevent_be/dto/request/ResetPasswordRequest.java
+++ src/main/java/com/example/myevent_be/dto/request/ResetPasswordRequest.java
@@ -9,6 +9,7 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ResetPasswordRequest {
+    String email;
+    String code;
     String newPassword;
-    String confirmPassword;
 }
